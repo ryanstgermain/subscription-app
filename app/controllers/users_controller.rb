617987a7f5840b3f2a270ebb7bf0789c3618a631
ryanstgermain <ryanjoel26@gmail.com>
2019-05-17 14:43:17 -0600

    got payments to successfuly go through

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def info
     @subscription = current_user.subscription
   end
+
+  def charge
+    token = params["stripeToken"]
+    customer = Stripe::Customer.create(
+      source: token,
+      plan: 'mysubscriptionlevel1',
+      email: current_user.email
+    )
+
+    current_user.subscription.stripe_user_id = customer.id
+    current_user.subscription.active = true
+    current_user.subscription.save
+    
+    redirect_to users_info_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/users/info', to: 'users#info'
+  post '/users/charge', to: 'users#charge'
   devise_for :users
   root to: "home#index"
   resources :publications, only: [:index, :show]
