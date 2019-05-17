0c03e3dc4d25579e92567933f3124e1dbe1f89c2
ryanstgermain <ryanjoel26@gmail.com>
2019-05-17 15:10:37 -0600

    made it so users can cancel a subscription

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def cancel_subscription
+    @stripe_customer = Stripe::Customer.retrieve(current_user.subscription.stripe_user_id)
+    @stripe_subscription = @stripe_customer.subscriptions.first
+    @stripe_subscription.delete
+    current_user.subscription.active = false
+    current_user.subscription.save
+    redirect_to users_info_path
+  end
+
   def charge
     token = params["stripeToken"]
     customer = Stripe::Customer.create(
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/users/info', to: 'users#info'
   post '/users/charge', to: 'users#charge'
+  get '/users/cancel_subscription', to: 'users#cancel_subscription'
   devise_for :users
   root to: "home#index"
   resources :publications, only: [:index, :show]
